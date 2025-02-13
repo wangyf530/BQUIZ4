5f79e1070f8d7c3eae22f1257bfcc4bd237fa1d2
yf wang <wangyf530@gmail.com>
2025-02-13 09:09:45 +0800

    管理權限設置頁面

--- api/db.php
+++ api/db.php
@@ -22,7 +22,7 @@ class DB
         if (!empty($arg[0]) && is_array($arg[0])) {
             $tmp = $this->a2s($arg[0]);
             $sql .= " WHERE " . join(" && ", $tmp);
-        } else if (!is_array($arg[0])) {
+        } else if (isset($arg[0]) && is_string($arg[0])) {
             // 如果第一個不是空的但不是陣列
             $sql .= $arg[1];
         }
@@ -113,8 +113,11 @@ class DB
     {
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
-
-    function fetch_all() {}
+    
+    function fetch_all($sql) {
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        
+    }
 }
 function to($url)
 {
